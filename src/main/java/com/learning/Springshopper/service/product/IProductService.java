300660ecde30e91df8299ac9eb2fc817d6713f28
Miten Patel <patelmit3n@gmail.com>
2024-10-01 21:47:29 -0400

    Update Interface to add AddProductRequest

--- src/main/java/com/learning/Springshopper/service/product/IProductService.java
+++ src/main/java/com/learning/Springshopper/service/product/IProductService.java
@@ -1,11 +1,12 @@
 package com.learning.Springshopper.service.product;
 
 import com.learning.Springshopper.model.Product;
+import com.learning.Springshopper.request.AddProductRequest;
 
 import java.util.List;
 
 public interface IProductService {
-    Product addProduct(Product product);
+    Product addProduct(AddProductRequest product);
     Product getProductById(Long id);
     void deleteProductById(Long id);
     void updateProduct(Product product, Long productId);
