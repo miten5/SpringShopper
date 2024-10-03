3a8b7f54370d22a08272962d835c432499689bfd
Miten Patel <patelmit3n@gmail.com>
2024-10-02 22:00:58 -0400

    Code refector

--- src/main/java/com/learning/Springshopper/service/product/IProductService.java
+++ src/main/java/com/learning/Springshopper/service/product/IProductService.java
@@ -2,6 +2,7 @@ package com.learning.Springshopper.service.product;
 
 import com.learning.Springshopper.model.Product;
 import com.learning.Springshopper.request.AddProductRequest;
+import com.learning.Springshopper.request.UpdateProductRequest;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface IProductService {
     Product addProduct(AddProductRequest product);
     Product getProductById(Long id);
     void deleteProductById(Long id);
-    void updateProduct(Product product, Long productId);
+    Product updateProduct(UpdateProductRequest product, Long productId);
     List<Product> getAllProducts();
     List<Product> getProductsByCategory(String category);
     List<Product> getProductsByBrand(String brand);
